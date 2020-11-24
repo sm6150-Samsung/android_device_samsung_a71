4e3cc7654f7661cad04ddf5ae96d8d34689dd1b0
LuK1337 <priv.luk@gmail.com>
2020-11-25 01:01:25 +0700

    a71: We ship with 10
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- device.mk
+++ device.mk
@@ -11,6 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Inherit common device configuration
 $(call inherit-product, device/samsung/sm6150-common/common.mk)
 
+PRODUCT_SHIPPING_API_LEVEL := 29
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
 
