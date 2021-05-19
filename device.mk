a4c85ca4b9d996cac57472fdc2bf36f3985c6a83
Faiz Authar <faizauthar@gmail.com>
2021-05-19 17:16:27 +0700

    a71: initial tree

---
+++ device.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/samsung/a71
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit common device configuration
+$(call inherit-product, device/samsung/sm6150-common/common.mk)
+
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_SUPER_PARTITION := false
+
+# Overlays
+PRODUCT_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
+
+# Init
+PRODUCT_PACKAGES += \
+    fstab.qcom
